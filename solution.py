0907c61a0a624081702f23240cbe7a8846b85d65
Jordan Huang <z5418948@unsw.edu.au>
2024-07-20 15:06:13 +1000

    wrote some comments for a potential structure

--- solution.py
+++ solution.py
@@ -48,7 +48,13 @@ import json
 # calls to get_action
 state = {}
 
+# cost constants
+COST_LIGHT = 1
+COST_PERSON_NO_LIGHT = 4
+
+# Initialising classes, variables, etc.
 # params = pd.read_csv(...)
+# pgm = ProbabilisticGraphicalModel(params) (idk)
 
 def get_action(sensor_data):
     # declare state as a global variable so it can be read and modified within this function
@@ -56,6 +62,22 @@ def get_action(sensor_data):
     #global params
 
     # TODO: Add code to generate your chosen actions, using the current state and sensor_data
+
+    # Step 1: Pass in the sensor data to the PGM, to set evidence
+    # pgm.update_data(sensor_data)
+    
+    # Step 2: Query the pgm to get the expected number of people in each area? Maybe?
+    # info = pgm.query()
+
+    # Step 3: Use this expected num. people per area to create and return our actions_dict
+    # return info_to_actions(info)
+
+    # Other considerations
+    # 1. We might want to incorporate things other than just the sensor data into our model
+    #    e.g. time of day, certain movement behaviours and patterns, hypothesised room functions
+    # 2. Maybe needs more classes? Something to process the sensor data into a form which 
+    #    can just be passed to the pgm.
+    # 3. Uh is this a Gaussian Model
 
     actions_dict = {'lights1': 'on', 'lights2': 'on', 'lights3': 'on', 'lights4': 'on',
                     'lights5': 'on', 'lights6': 'on', 'lights7': 'on', 'lights8': 'on',
@@ -66,4 +88,13 @@ def get_action(sensor_data):
                     'lights25': 'on', 'lights26': 'on', 'lights27': 'on', 'lights28': 'on',
                     'lights29': 'on', 'lights30': 'on', 'lights31': 'on', 'lights32': 'on',
                     'lights33': 'on', 'lights34': 'on'}
+    return actions_dict
+
+# Presumably, we're expecting an output of the expected number of people in each room.
+# If there's more than or equal to 0.25 people in the room, turn the light on.
+# Right now, probs accepts a dictionary of form {'r1': 0.25, 'r2': 0.3, 'r3': 0.1, 'r4': 1, ..., 'c1': 2, 'c2': 0.2, 'outside': '11'}
+# and returns a dictionary of form {'lights1': 'on', 'lights2': 'on', ..., 'lights34': 'off'}
+def info_to_actions(info):
+    actions_dict = {'lights' + str(match.group(1)) : 'off' if nPeople < COST_LIGHT / COST_PERSON_NO_LIGHT else 'on'
+                    for area, nPeople in info.items() if (match := re.match(r'r(\d+)', area))}
     return actions_dict
