55fb1f620266133e49eff17a7bea5fc024c33e1f
Jordan Huang <z5418948@unsw.edu.au>
2024-08-04 14:09:45 +1000

    motion sensors sorta done, some hardcoded constants

--- motion_sensor.py
+++ motion_sensor.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from gaussian_factor import GaussianFactor
 
 class MotionSensor:
@@ -5,19 +7,34 @@ class MotionSensor:
     def __init__(self, area):
         self.area = area
 
-    # TODO: Assess the reliability of the sensors, using the training data.
     def update(self, data):
         self.response = data
 
     # Apply evidence on the room distributions given the currently stored evidence.
     # Takes in the current means and vars, and returns the new means and vars.
     def apply_evidence(self, means, vars, t_m):
+        # return means, vars
         if self.response == None:
             return means, vars
         
+        observation = means[self.area]
         if self.response == 'no motion':
-            ...
+            # If we get no motion, we convert this into an observation that there were 0 people in the room.
+            observation = 0
         elif self.response == 'motion':
-            ...
+            # If we get yes motion, we convert this into an observation that there were people in the room
+            # We only slightly skew up our belief if our existing belief was already positive,
+            # but we skew up our belief more significantly if our existing belief was close to 0
+            #
+            # A nice way to do this is by skewing up using a decreasing function: lambda * exp(-{existing belief})
+            observation = means[self.area] + 0.5 * np.exp(-means[self.area])
+        
+        prior = GaussianFactor(('num_ppl',), mu=means[self.area], sigma=vars[self.area])
+        motion = GaussianFactor(('obs_num_ppl', 'num_ppl',), beta=[1], b_mean=0, b_var=0.09)
+        joint = prior * motion
+        final = joint.evidence(obs_num_ppl=observation)
+
+        means[self.area] = (final.mean()).reshape(1)[0]
+        vars[self.area] = (final.covariance()).reshape(1)[0]
         
         return means, vars
